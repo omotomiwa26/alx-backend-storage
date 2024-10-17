92ce568f4156c0e7a74f86ac8fc8a500989bc017
omotomiwa26 <omotomiwa26@gmail.com>
2024-10-17 02:14:23 +0100

    0. We are all unique!

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,12 +1,9 @@
 -- This SQL creates a database hoberton if not exists
 -- This SQL script creates a table users in database holberton if not exists
-
 CREATE DATABASE IF NOT EXISTS holberton;
 USE holberton;
-
 CREATE TABLE IF NOT EXISTS users (
-id INT UNIQUE AUTO_INCREMENT NOT NULL,
-PRIMARY KEY(id),
-email VARCHAR(255) NOT NULL UNIQUE,
-name VARCHAR(255)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY(id),
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
 );
