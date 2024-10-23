2c62f598890fb72b222da9335a44beb9267b0b9c
omotomiwa26 <omotomiwa26@gmail.com>
2024-10-23 02:09:43 +0100

    9. Insert a document in Python

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+    This module inserts a new document
+    in a collection
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+        mongo_collection will be the pymongo collection object
+        Returns the new _id
+    """
+    new_document = mongo_collection.insert_one(kwargs)
+    return new_document.inserted_id
