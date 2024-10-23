b71c630a322e483354cb206cff0c47ea05421594
omotomiwa26 <omotomiwa26@gmail.com>
2024-10-23 02:05:41 +0100

    8. List all documents in Python

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    This function that lists all documents
+"""
+
+
+def list_all(mongo_collection):
+    """
+        empty list if no document in the collection
+    """
+    return mongo_collection.find()
